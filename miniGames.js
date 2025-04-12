e6f9a8b8431b43aa795567a511c029891fe41a97
Nikkalas11 <nikolaj.sever11@gmail.com>
2025-04-12 15:06:41 +0300

    added branch hw6 & connected index_hw6.js

--- miniGames.js
+++ miniGames.js
@@ -4,19 +4,20 @@ function guessNumber() {
   let userNumber = prompt(`Попробуйте отгадать число от 1 до 100`);
   let attempt = 0;
   while (true) {
-    attempt ++;
-    if (isNaN(userNumber))  {
-        userNumber = prompt(`Вы ввели не правильное значение. Попробуйте снова`);
-    }  else if (!Boolean(userNumber)) {
-        alert(`До свидания. Ха - ха - ха`)
-        break
-      }
-     else if (userNumber > randomNumber) {
+    attempt++;
+    if (isNaN(userNumber)) {
+      userNumber = prompt(`Вы ввели не правильное значение. Попробуйте снова`);
+    } else if (!Boolean(userNumber)) {
+      alert(`До свидания. Ха - ха - ха`);
+      break;
+    } else if (userNumber > randomNumber) {
       userNumber = prompt(`Ваше число ${userNumber} - больше секретного`);
     } else if (userNumber < randomNumber) {
       userNumber = prompt(`Ваше число ${userNumber} - меньше секретного`);
     } else {
-      alert(`Вы угадали задуманное число ${randomNumber} за ${attempt} попыток`);
+      alert(
+        `Вы угадали задуманное число ${randomNumber} за ${attempt} попыток`
+      );
       break;
     }
   }
